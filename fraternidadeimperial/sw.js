e3713bf71204f157218b1577a5f5dcbef05426e2
imaginiista <imaginiista@gmail.com>
2026-04-27 15:54:53 -0300

    Add files via upload

--- fraternidadeimperial/sw.js
+++ fraternidadeimperial/sw.js
@@ -1,15 +1,16 @@
-const CACHE_NAME = 'fraternidade-264-v1';
+const CACHE_NAME = 'fraternidade-264-v2';
 const urlsToCache = [
-  './',
-  './index.html'
+  '/fraternidadeimperial/'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
+        console.log('Cache aberto com sucesso');
         return cache.addAll(urlsToCache);
       })
+      .catch(err => console.log('Erro no cache:', err))
   );
 });
 
